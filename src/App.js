909aabe4bf2ab0a2fc037c1f9eed8535cde911bc
Wen_YG <361583983@qq.com>
2021-08-13 17:42:44 +0900

    homework4

--- src/App.js
+++ src/App.js
@@ -32,13 +32,6 @@ const App = () => {
     setExpenses((prevExpenses) => [expense, ...prevExpenses]);
   };
 
-  // return React.createElement(
-  //   'div',
-  //   {},
-  //   React.createElement('h2', {}, "Let's get started!"),
-  //   React.createElement(Expenses, { items: expenses })
-  // );
-
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler} />
