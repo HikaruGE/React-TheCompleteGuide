3e79f3e31798043cb45c5aa989a06a7f02f74005
Wen_YG <361583983@qq.com>
2021-08-13 17:53:21 +0900

    code improved

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -6,7 +6,6 @@ const ExpenseForm = (props) => {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
-  const [addingExpenseFlag, setAddingExpenseFlag] = useState(false);
 
   const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
@@ -34,57 +33,49 @@ const ExpenseForm = (props) => {
     setEnteredAmount("");
     setEnteredDate("");
 
-    onChangeExpenseFlagHandler();
+    props.onChangeExpenseFlag();
   };
 
-  const onChangeExpenseFlagHandler = () => {
-    setAddingExpenseFlag((addingExpenseFlag) => !addingExpenseFlag);
-  };
-
-  if (!addingExpenseFlag) {
-    return (
-      <button onClick={onChangeExpenseFlagHandler}>New Add Expense</button>
-    );
-  } else {
-    return (
-      <form onSubmit={submitHandler}>
-        <div className='new-expense__controls'>
-          <div className='new-expense__control'>
-            <label>Title</label>
-            <input
-              type='text'
-              value={enteredTitle}
-              onChange={titleChangeHandler}
-            />
-          </div>
-          <div className='new-expense__control'>
-            <label>Amount</label>
-            <input
-              type='number'
-              min='0.01'
-              step='0.01'
-              value={enteredAmount}
-              onChange={amountChangeHandler}
-            />
-          </div>
-          <div className='new-expense__control'>
-            <label>Date</label>
-            <input
-              type='date'
-              min='2019-01-01'
-              max='2022-12-31'
-              value={enteredDate}
-              onChange={dateChangeHandler}
-            />
-          </div>
+  return (
+    <form onSubmit={submitHandler}>
+      <div className='new-expense__controls'>
+        <div className='new-expense__control'>
+          <label>Title</label>
+          <input
+            type='text'
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
-        <div className='new-expense__actions'>
-          <button onClick={onChangeExpenseFlagHandler}>Cancel</button>
-          <button type='submit'>Add Expense</button>
+        <div className='new-expense__control'>
+          <label>Amount</label>
+          <input
+            type='number'
+            min='0.01'
+            step='0.01'
+            value={enteredAmount}
+            onChange={amountChangeHandler}
+          />
         </div>
-      </form>
-    );
-  }
+        <div className='new-expense__control'>
+          <label>Date</label>
+          <input
+            type='date'
+            min='2019-01-01'
+            max='2022-12-31'
+            value={enteredDate}
+            onChange={dateChangeHandler}
+          />
+        </div>
+      </div>
+      <div className='new-expense__actions'>
+        <button type='button' onClick={props.onChangeExpenseFlag}>
+          Cancel
+        </button>
+        <button type='submit'>Add Expense</button>
+      </div>
+    </form>
+  );
 };
 
 export default ExpenseForm;
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import ExpenseForm from './ExpenseForm';
-import './NewExpense.css';
+import ExpenseForm from "./ExpenseForm";
+import "./NewExpense.css";
 
 const NewExpense = (props) => {
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
-      id: Math.random().toString()
+      id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
   };
 
+  const [addingExpenseFlag, setAddingExpenseFlag] = useState(false);
+  const onChangeExpenseFlagHandler = () => {
+    setAddingExpenseFlag((addingExpenseFlag) => !addingExpenseFlag);
+  };
+
   return (
     <div className='new-expense'>
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!addingExpenseFlag && (
+        <button type='button' onClick={onChangeExpenseFlagHandler}>
+          New Add Expense
+        </button>
+      )}
+      {addingExpenseFlag && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onChangeExpenseFlag={onChangeExpenseFlagHandler}
+        />
+      )}
     </div>
   );
 };
